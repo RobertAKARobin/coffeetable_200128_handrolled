584d0b63a85d11273e309cb1301e36221042e588
RobertAKARobin <contact@robertakarobin.com>
2020-01-27 09:54:12 -0600

    Add typescript eslint

--- index.ts
+++ index.ts
@@ -1,51 +1,49 @@
-import { state } from './state';
-
-class Base {
+export class Base {
 
 }
 
-class Collection extends Base {
-	private _fields: Field[];
-	private _records: Record[];
+export class Collection extends Base {
+  private _fields: Field[];
+  private _records: Record[];
 }
 
-class Field extends Base {
-	private _collection: Collection;
-	private _index: number;
+export class Field extends Base {
+  private _collection: Collection;
+  private _index: number;
 }
 
-class Record extends Base {
-	private _collection: Collection;
-	private _data: any;
-	private _index: number;
-	private _values: Value[];
+export class Record extends Base {
+  private _collection: Collection;
+  private _data: any;
+  private _index: number;
+  private _values: Value[];
 }
 
-class Value extends Base {
-	private _data: any;
-	private _display: string;
-	private _field: Field;
-	private _record: Record;
+export class Value extends Base {
+  private _data: any;
+  private _display: string;
+  private _field: Field;
+  private _record: Record;
 }
 
 // Selections
 
-class Selection {
+export class Selection {
 
 }
 
-class CollectionSelection extends Selection {
-	private _collections: Collection[];
+export class CollectionSelection extends Selection {
+  private _collections: Collection[];
 }
 
-class RecordSelection extends Selection {
-	private _records: [];
+export class RecordSelection extends Selection {
+  private _records: [];
 }
 
-class FieldSelection extends Selection {
-	private _fields: Field[];
+export class FieldSelection extends Selection {
+  private _fields: Field[];
 }
 
-class ValueSelection extends Selection {
-	private _values: Value[];
+export class ValueSelection extends Selection {
+  private _values: Value[];
 }
